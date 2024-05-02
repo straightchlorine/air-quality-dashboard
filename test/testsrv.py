1ce06bff02d329188552d087ecc8d31eea105bbe
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-05-02 11:47:19 +0200

    testsrv: tidied up the code
    turned the testsrv into separate class

--- test/testsrv.py
+++ test/testsrv.py
@@ -2,37 +2,42 @@
 
 from flask import Flask, jsonify
 
-example_json = {
-    "nodemcu": {
-        "bmp180": {
-            "altitude": "149.56",
-            "pressure": "998.42",
-            "seaLevelPressure": "1016.34",
-            "temperature": "26.00"
-        },
-        "mq135": {
-            "aceton": "2.57",
-            "alcohol": "6.62",
-            "co": "28.88",
-            "co2": "412.10",
-            "nh4": "15.12",
-            "toulen": "3.14"
+class TestServer:
+    """A simple server to test the client with."""
+
+    srv : Flask # The Flask server instance
+
+    """The example JSON that the server will return when the /circumstances endpoint is called."""
+    def __init__(self):
+        self.srv = Flask(__name__)
+        @self.srv.route('/circumstances', methods=['GET'])
+        def get_circumstances():
+            return jsonify(self.example_json)
+
+    # Example JSON response from nodemcu
+    example_json = {
+        "nodemcu": {
+            "bmp180": {
+                "altitude": "149.56",
+                "pressure": "998.42",
+                "seaLevelPressure": "1016.34",
+                "temperature": "26.00"
+            },
+            "mq135": {
+                "aceton": "2.57",
+                "alcohol": "6.62",
+                "co": "28.88",
+                "co2": "412.10",
+                "nh4": "15.12",
+                "toulen": "3.14"
+            }
         }
     }
-}
 
-
-def server_setup():
-    srv = Flask(__name__)
-
-    @srv.route('/circumstances', methods=['GET'])
-    def get_circumstance():
-        return jsonify(data)
-
-
-
-def run_srv():
-    srv.run()
+    """Start the server."""
+    def run_testing_server(self):
+        self.srv.run()
 
 if __name__ == "__main__":
-    run_srv()
+    srv = TestServer()
+    srv.run_testing_server()
