bf0b8f42a16589bec13fd6546afbb8f256e56832
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-05-06 15:21:01 +0200

    dev_dashboard: very basic dashboard for testing
    really basic, things not pretty really

--- test/ui/dev_dashboard.py
+++ test/ui/dev_dashboard.py
@@ -1,113 +1,112 @@
 # Author: Piotr Krzysztof Lis - github.com/straightchlorine
 
-import asyncio
 import json
-from threading import Thread
-from dash import Dash, Input, Output, callback, dcc, html
+import multiprocessing
 import pandas as pd
-import plotly
-import pytest
+import asyncio
+import sys
 
-from reads.interface import InfluxDBInterface
+from dash import Dash, dcc, html, Input, Output, callback
+from pathlib import Path
 
-class DevelopmentDashboard:
-    """
-    A simple development server for testing purposes.
-    """
+# adding current directory to the path
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
 
-    srv : Dash # The Flask server instance
+from reads.interface import DatabaseInterface
+from test.dev_server import DevelopmentServer
 
-    def __init__(self):
-        """
-        Setting up the server as well as the handle.
-        """
-        # initialize the dash application
-        external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-        self.srv = Dash(__name__, external_stylesheets=external_stylesheets)
+# start the test server and specifiy the IP and port
+DevelopmentServer().run_test_server()
+test_dev_ip = 'localhost'
+test_dev_port = 5000
 
-        # defining the layouts
-        self.srv.layout = html.Div(
-            [
-                html.H1("InfluxDB Data Dashboard"),
-                dcc.Graph(id='live-update-graph'),
-                dcc.Interval(
-                    id='interval-component',
-                    interval=1*1000, # in milliseconds
-                    n_intervals=0
-                )
-            ]
+# load the secrets
+with open('secrets/secrets.json', 'r') as f:
+    secrets = json.load(f)
+
+# list of sensors and the parameters they measure
+sensors = {
+    "bmp180": ["altitude", "pressure", "temperature", "seaLevelPressure"],
+    "mq135": ["aceton", "alcohol", "co", "co2", "nh4", "toulen"]
+}
+
+# create the DatabaseInterface object
+interface = DatabaseInterface(secrets['host'],
+                              secrets['port'],
+                              secrets['token'],
+                              secrets['organization'],
+                              secrets['bucket'],
+                              sensors,
+                              test_dev_ip,
+                              test_dev_port,
+                              secrets['handle'])
+
+# data update task
+async def update_data():
+    # get the data
+    data = await interface.query_latest()
+    return data
+
+# initializing and setting up the dashboard
+dashboard_server = Dash(__name__)
+dashboard_server.layout = html.Div(
+    html.Div([
+        html.H4('NodeMCU Sensor Data'),
+        html.Div(id='live-update-text'),
+        dcc.Interval(
+            id='interval-component',
+            interval=1000,
+            n_intervals=0
         )
+    ])
+)
 
-    @callback(Output('live-update-graph', 'figure'),
-                Input('interval-component', 'n_intervals'))
-    def update_graph_live(n):
-        # measurement = asyncio.run(update_data())
+# live updating the text
+@callback(Output('live-update-text', 'children'),
+          Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+    measurement_dataframe : pd.DataFrame = asyncio.run(update_data())
 
-        # Create the graph with subplots
-        fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-        fig['layout']['margin'] = {
-            'l': 30, 'r': 10, 'b': 30, 't': 10
-        }
-        fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
+    if not measurement_dataframe.empty:
+        time = measurement_dataframe['time'].iloc[-1]
+        altitude = measurement_dataframe['altitude'].iloc[-1]
+        pressure = measurement_dataframe['pressure'].iloc[-1]
+        temperature = measurement_dataframe['temperature'].iloc[-1]
+        sea_level_pressure = measurement_dataframe['seaLevelPressure'].iloc[-1]
+        aceton = measurement_dataframe['aceton'].iloc[-1]
+        alcohol = measurement_dataframe['alcohol'].iloc[-1]
+        co = measurement_dataframe['co'].iloc[-1]
+        co2 = measurement_dataframe['co2'].iloc[-1]
+        nh4 = measurement_dataframe['nh4'].iloc[-1]
+        toulen = measurement_dataframe['toulen'].iloc[-1]
+        style = {'padding': '5px', 'fontSize': '16px'}
+        return [
+            html.Span('Time: {0}'.format(time), style=style),
+            html.Span('Altitude: {0:.2f}'.format(altitude), style=style),
+            html.Span('Pressure: {0:.2f}'.format(pressure), style=style),
+            html.Span('Temperature: {0:.2f}'.format(temperature), style=style),
+            html.Span('Sea Level Pressure: {0:.2f}'.format(sea_level_pressure), style=style),
+            html.Span('Aceton: {0:.2f}'.format(aceton), style=style),
+            html.Span('Alcohol: {0:.2f}'.format(alcohol), style=style),
+            html.Span('CO: {0:.2f}'.format(co), style=style),
+            html.Span('CO2: {0:.2f}'.format(co2), style=style),
+            html.Span('NH4: {0:.2f}'.format(nh4), style=style),
+            html.Span('Toulen: {0:.2f}'.format(toulen), style=style),
+        ]
+    else:
+        return [html.Span('probing data...')]
 
-        # fig.append_trace({
-        #     'x': measurement['time'],
-        #     'y': measurement['temperature'],
-        #     'name': 'Altitude',
-        #     'mode': 'lines+markers',
-        #     'type': 'scatter'
-        # }, 1, 1)
-        # fig.append_trace({
-        #     'x': measurement['time'],
-        #     'y': measurement['pressure'],
-        #     'text': measurement['time'],
-        #     'name': 'Longitude vs Latitude',
-        #     'mode': 'lines+markers',
-        #     'type': 'scatter'
-        # }, 2, 1)
-
-        return fig
-    
-    def start_dashboard(self):
-        self.srv.run_server(debug=True, host='localhost', port=8001)
-
-    def run_dashboard(self):
-        """
-        Start the server on a separate thread.
-        """
-        server_thread = Thread(target=self.srv.start_dashboard)
-        server_thread.start()
+def run():
+    dashboard_server.scripts.config.serve_locally = True
+    dashboard_server.run_server(
+        port=8050,
+        debug=False,
+        processes=4,
+        threaded=False
+    )
 
 if __name__ == "__main__":
-    srv = DevelopmentDashboard()
-    srv.run_dashboard()
-
-
-
-
-class Dashboard:
-
-    # running the test server and specifying the IP and port
-    test_dev_ip = 'localhost'
-    test_dev_port = 5000
-
-    # loading the secrets
-    with open('secrets/secrets.json', 'r') as f:
-        secrets = json.load(f)
-
-    # list of attached sensors and their parameters
-    sensors = {
-        "bmp180": ["altitude", "pressure", "temperature", "seaLevelPressure"],
-        "mq135": ["aceton", "alcohol", "co", "co2", "nh4", "toulen"]
-    }
-
-    # connecting to the InfluxDB instance via the InfluxDBInterface
-    interface = InfluxDBInterface(secrets['host'],
-                                secrets['port'],
-                                secrets['token'],
-                                secrets['organization'],
-                                secrets['bucket'],
-                                sensors,
-                                test_dev_ip,
-                                test_dev_port,
-                                secrets['handle'])
+    server_process = multiprocessing.Process(target=run, name='dash')
+    server_process.start()
+    interface.enable_fetching()
