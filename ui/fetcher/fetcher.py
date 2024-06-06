b654e3d6c389d57e813cfabfa2b16f2a629368e1
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-06-06 17:22:22 +0200

    fetcher: separate fetcher

---
+++ ui/fetcher/fetcher.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import json
+
+from ahttpdc.reads.interface import DatabaseInterface
+
+# load the secrets
+with open('secrets/secrets.json', 'r') as f:
+    secrets = json.load(f)
+
+sensors = {
+    'bmp180': ['altitude', 'pressure', 'seaLevelPressure'],
+    'mq135': ['aceton', 'alcohol', 'co', 'co2', 'nh4', 'toulen'],
+    'ds18b20': ['temperature'],
+    'dht22': ['humidity'],
+}
+
+# create the DatabaseInterface object
+interface = DatabaseInterface(
+    secrets['host'],
+    secrets['port'],
+    secrets['token'],
+    secrets['organization'],
+    secrets['bucket'],
+    sensors,
+    secrets['dev_ip'],
+    secrets['dev_port'],
+    secrets['handle'],
+)
+
+if __name__ == '__main__':
+    interface.enable_fetching()
